48bb46e8b752b4a932d9f9b0acf5f09783d4a50e
Brandon Hopkins <bph.accounts@proton.me>
2025-11-30 18:23:58 -0800

    fedxps: 2025-11-30 18:23:45

---
+++ nvim/after/ftplugin/make.lua
@@ -0,0 +1,8 @@
+
+-- ~/.config/nvim/after/ftplugin/make.lua
+
+-- Use literal tabs for Makefiles
+vim.opt_local.expandtab   = false
+vim.opt_local.tabstop     = 8
+vim.opt_local.shiftwidth  = 8
+vim.opt_local.softtabstop = 0
